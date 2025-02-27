786dbaa3d64c19c7ce8b617dba4ad87efae7d83c
if-romario <romariojdosantos@gmail.com>
2025-02-27 20:05:52 -0300

    Simple and easy dark mode toggle with TailwindCSS 4.0+

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+  document.documentElement.classList.toggle(
+    "dark",
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+
+  let isDark = document.getElementById("theme-toggle");
+
+  // const clickAudio = new Audio("./click.mp3");
+  const clickAudio = new Audio(
+    "https://www.myinstants.com/media/sounds/assault_cannon.mp3"
+  );
+
+  isDark.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      document.documentElement.classList.add("dark");
+      clickAudio.play();
+    } else {
+      document.documentElement.classList.remove("dark");
+      clickAudio.play();
+    }
+    localStorage.setItem("theme", event.target.checked ? "dark" : "light");
+    console.log("Theme set to:", isDark ? "dark" : "light");
+    console.log("Current classes on html:", document.documentElement.className);
+  });
+};
